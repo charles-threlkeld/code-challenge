b3bf3f7efe6ae79c24cdba81ef178eff7d54423b
Chas Threlkeld <charles.threlkeld@protonmail.com>
2024-07-18 14:29:09 -0500

    Step 4: Display results from the API

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -19,6 +19,35 @@ submitButton.onclick = async function(event) {
   try {
     const response = await fetch(`${url}?${queryTerms}`);
     console.log(response)
+    if (!response.ok) {
+      if (response.status == 400) {
+        alert(`${response.statusText}: Failed to parse this address`);
+      } else {
+        // TODO: Handle other failures?
+      }
+    } else {
+      let responseData = await response.json()
+
+      // Set the headline
+      document.getElementById("parse-type").textContent = responseData.address_type;
+
+      // Build the table
+      let table = document.getElementById("address-table");
+      table.innerHTML = blankTable;
+      let addressParts = responseData.address_components;
+      for (let index in addressParts) {
+        let field = addressParts[index][0];
+        let value = addressParts[index][1];
+
+        let nextRow = table.insertRow(-1);
+        let fieldData = nextRow.insertCell(0);
+        let valueData = nextRow.insertCell(1);
+  
+        fieldData.appendChild(document.createTextNode(field));
+        valueData.appendChild(document.createTextNode(value));
+      }
+      results.style.display = "block";
+    }
   } catch (error) {
     console.error(`${error} in response to query at ${url}?{queryTerms}`);
   }
